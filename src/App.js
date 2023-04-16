96a0bc6ba574e3be5ebb00b1990de5b52276aa51
Aditya Gupta <adityagupta11219@gmail.com>
2023-04-16 18:08:29 +0530

    added changes

--- src/App.js
+++ src/App.js
@@ -25,6 +25,25 @@ const Navbar = () => {
 					</button>
 				</div>
 				<ul className="hidden lg:flex lg:items-center lg:justify-end grow mr-4">
+				<li className="mr-4">
+						<a
+							className="text-gray-600 dark:text-gray-300 hover:text-gray-100 px-4 py-2"
+							href="https://github.com/1pushpak1/Video-Calling-app-for-Deafeneds"
+						>
+						<div className="flex">
+							<p className="pt-1.5 pr-2">Github</p>
+							<svg className="icon github" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512"><path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"/></svg>
+							</div></a>
+					</li>
+					<li>
+						<a
+							className="text-gray-600 dark:text-gray-300 hover:text-gray-100 px-4 py-2"
+							href="https://drive.google.com/file/d/1xo7RbIZW9gg_rnmAkkpgo9YwkrmTmX6h/view?usp=share_link"
+						>
+						<div className="flex">
+							<p className="pt-1.5 pr-2">Video Demo</p>
+							<svg className="icon github" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M464 256A208 208 0 1 0 48 256a208 208 0 1 0 416 0zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zM188.3 147.1c7.6-4.2 16.8-4.1 24.3 .5l144 88c7.1 4.4 11.5 12.1 11.5 20.5s-4.4 16.1-11.5 20.5l-144 88c-7.4 4.5-16.7 4.7-24.3 .5s-12.3-12.2-12.3-20.9V168c0-8.7 4.7-16.7 12.3-20.9z"/></svg></div></a>
+					</li>
 					<li>
 						<a
 							className="text-gray-600 dark:text-gray-300 hover:text-gray-100 px-4 py-2"
@@ -97,52 +116,39 @@ const Problem = () => {
 			id="problem"
 			className="py-4 lg:flex lg:justify-center bgrnd layer3"
 		>
-			<div className="overflow-hidden mt-32 lg:mx-8 lg:flex lg:max-w-6xl lg:w-full lg:shadow-md lg:rounded-xl lg:justify-between">
-				<div className="lg:w-1/2">
-					<div className="h-70 bg-cover ">
-						<img
-							className="object-cover w-full h-full"
-							src="https://images.unsplash.com/photo-1552581234-26160f608093?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"
-							alt=""
-						/>
-					</div>
-				</div>
+			
+				
 
-				<div className="max-w-xl ml-12 px-6 py-2 lg:max-w-5xl lg:w-1/2">
+				<div className="max-w-xl ml-12 px-6  lg:max-w-5xl lg:w-1/2">
 					<h2 className="text-5xl font-semibold text-gray-800 dark:text-white ">
 						The Problem
 					</h2>
 
-					<p className="mt-4 text-2xl dark:text-gray-300">
-						The problem that it aims to address is the communication barrier
-						between the deaf and hard of hearing community and the hearing
-						world. Many deaf and hard of hearing individuals use sign language
-						as their primary means of communication. However, not everyone is
-						fluent in sign language, which can make communication difficult
-						during live video conferences.
+					<p className="mt-6 text-2xl dark:text-gray-300">
+					Communication between deaf and hard of hearing people and hearing people is often hindered by the lack of a common language.</p>
+					<p className="mt-6 text-2xl dark:text-gray-300"> Many deaf and hard of hearing people use sign language as their primary means of communication, but not everyone is fluent in sign language.</p>
+					<p className="mt-6 text-2xl dark:text-gray-300"> This can create barriers during live video conferences, which are increasingly common in various settings such as education, work, and social interactions. These barriers can lead to exclusion, misunderstanding, and frustration for both parties.</p>
+
+					<p className="mt-6 text-2xl dark:text-gray-300">Our project aims to bridge this communication gap by providing real-time captioning for sign language during live video conferences.
 					</p>
 				</div>
-			</div>
+			
 		</section>
 	);
 };
 const Solution = () => {
 	return (
-		<section id="solution" className="bgrnd layer1 pt-16">
+		<section id="solution" className="bgrnd layer1 pt-8">
 			<div className="container flex flex-col items-center px-4 py-12 mx-auto text-center">
 				<h2 className="text-3xl font-bold tracking-tight text-gray-800 xl:text-5xl dark:text-white mb-4">
 					Solution Suggested
 				</h2>
 
 				<p className="block max-w-4xl mt-4 text-2xl text-gray-500 dark:text-gray-300">
-					The solution to this problem involves using Python and machine
-					learning techniques to train a model that can recognize and interpret
-					hand gestures in real-time. OpenCV, a popular computer vision library,
-					can be used to process the video feed and extract relevant features
-					from the hand gestures. These features can then be fed into a machine
-					learning model that has been trained to recognize and interpret
-					different sign language gestures. The output of the model can then be
-					used to generate real-time captions during live video conferences.
+				We will use Python and machine learning techniques to train a model that can recognize and interpret hand gestures in real-time. We will also use Flutter, React, and NodeJS to build a cross-platform mobile and web application that can generate captions from sign language and vice versa. Our project will enable seamless communication and inclusion for deaf and hard of hearing people and hearing people during live video conferences.
+				</p>
+				<p className="block max-w-4xl mt-4 text-2xl text-gray-500 dark:text-gray-300">
+				We will evaluate our project by testing its accuracy, speed, usability, and user satisfaction. We will also compare our project with existing solutions such as interpreters or transcription services. Our project will contribute to the fields of computer vision, machine learning, and accessibility.
 				</p>
 			</div>
 		</section>
@@ -154,7 +160,7 @@ const Advantages = () => {
 			id="adv"
 			className="max-w-full mx-auto px-10 md:px-0 py-10 bgrnd layer4"
 		>
-			<div className="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl ">
+			<div className="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl pt-16 ">
 				<div className="max-w-2xl mx-auto lg:text-center pt-16">
 					<h2 className="text-5xl font-bold leading-tight text-black dark:text-white sm:text-4xl lg:text-5xl">
 						Advantages
@@ -163,52 +169,68 @@ const Advantages = () => {
 				<div className="max-w-3xl mx-auto mt-8 md:mt-16 grid grid-cols-1 md:grid-cols-2 gap-6">
 					<div>
 						<h2 className="font-semibold text-3xl text-black dark:text-white">
-							Improved communication
+						Improved communication and understanding
 						</h2>
-						<p className="text-lg leading-6 tracking-wide text-gray-500 mt-6">
-							It will allow for seamless communication between the deaf and hard
-							of hearing community and the hearing world during live video
-							conferences
+						<p className="text-lg leading-6 tracking-wide text-gray-400 mt-6">
+						It improves communication and understanding between deaf and hard of hearing people and hearing people during live video conferences.
 						</p>
 					</div>
 					<div>
 						<h2 className="font-semibold text-3xl text-black dark:text-white">
-							Inclusion
+						Inclusion and accessibility
 						</h2>
-						<p className="text-lg leading-6 tracking-wide text-gray-500 mt-6">
-							By providing real-time captions, it ensures that everyone has a
-							voice and can fully participate in the conversation.
+						<p className="text-lg leading-6 tracking-wide text-gray-400 mt-6">
+						It promotes inclusion and accessibility for deaf and hard of hearing people by providing them with a voice and a tool to participate in various settings such as education, work, and social interactions.
 						</p>
 					</div>
 					<div>
 						<h2 className="font-semibold text-3xl text-black dark:text-white">
-							Increased accessibility
+						Reduced need for intermediaries
 						</h2>
-						<p className="text-lg leading-6 tracking-wide text-gray-500 mt-6">
-							It will make video conferences more accessible to the deaf and
-							hard of hearing community.
+						<p className="text-lg leading-6 tracking-wide text-gray-400 mt-6">
+						It reduces the need for interpreters or transcription services, which can be costly, scarce, or unavailable.
 						</p>
 					</div>
 					<div>
 						<h2 className="font-semibold text-3xl text-black dark:text-white">
-							Enhanced understanding
+						Flexibility and spontaneity
 						</h2>
-						<p className="text-lg leading-6 tracking-wide text-gray-500 mt-6">
-							Real-time captions can help to improve understanding and reduce
-							misunderstandings during video conferences.
+						<p className="text-lg leading-6 tracking-wide text-gray-400 mt-6">
+						It allows for more flexibility and spontaneity in the presentation, as the speaker does not have to stick to a script or pre-recorded video.
 						</p>
 					</div>
-				</div>
-				<div className="align-center">
-					<h2 className="font-semibold text-3xl text-black dark:text-white">
+					<div>
+						<h2 className="font-semibold text-3xl text-black dark:text-white">
+						Enhanced learning and retention
+						</h2>
+						<p className="text-lg leading-6 tracking-wide text-gray-400 mt-6">
+						It enhances the learning and retention of information by providing both visual and auditory cues.</p>
+					</div>
+					<div>
+						<h2 className="font-semibold text-3xl text-black dark:text-white">
+						Language diversity
+						</h2>
+						<p className="text-lg leading-6 tracking-wide text-gray-400 mt-6">
+						It benefits people who are not fluent in sign language or the spoken language of the presentation, as they can read the captions in their preferred language.</p>
+					</div>
+					<div>
+						<h2 className="font-semibold text-3xl text-black dark:text-white">
+						Noise adaptation
+						</h2>
+						<p className="text-lg leading-6 tracking-wide text-gray-400 mt-6">
+						It helps people who are viewing the presentation in a noisy or noiseless environment, as they can adjust the volume or mute the audio without missing any information.</p>
+					</div>
+					<div>
+						<h2 className="font-semibold text-3xl text-black dark:text-white">
 						Empowerment
-					</h2>
-					<p className="text-lg leading-6 tracking-wide text-gray-500 mt-6">
+						</h2>
+						<p className="text-lg leading-6 tracking-wide text-gray-400 mt-6">
 						It will empower the deaf and hard of hearing community by providing
 						them with a tool to communicate more effectively with the hearing
-						world.
-					</p>
+						world.</p>
+					</div>
 				</div>
+				
 			</div>
 		</section>
 	);
@@ -284,26 +306,41 @@ const Footer = () => {
 				</div>
 
 				<div className="mt-12 max-w-4xl mx-auto flex items-center justify-center flex-wrap">
-					<div className="dark:bg-gray-300 rounded-md h-20 w-48 my-4 mx-4">
-						<h3 className="text-3xl font-semibold text-black mt-4">
+				<a href="https://www.linkedin.com/in/adityagupta11219/">
+					<div className="dark:bg-gray-300 rounded-md h-20 w-48 my-4 mx-4 flex ">
+						<h3 className="text-3xl font-semibold text-black mt-5 mx-6">
 							Aditya
 						</h3>
+						<svg className="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"> <path d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z"/></svg>
 					</div>
-					<div className="dark:bg-gray-300 rounded-md h-20 w-48 my-4 mx-4">
-						<h3 className="text-3xl font-semibold text-black mt-4">
+					</a>
+					<a href="https://www.linkedin.com/in/yatharth-gautam-a01802210/">
+					<div className="dark:bg-gray-300 rounded-md h-20 w-48 my-4 mx-4 flex">
+						<h3 className="text-3xl font-semibold text-black mt-5 mx-4">
 							Yatharth
 						</h3>
+						<svg className="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"> <path d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z"/></svg>
+					
 					</div>
-					<div className="dark:bg-gray-300 rounded-md h-20 w-48 my-4 mx-4">
-						<h3 className="text-3xl font-semibold text-black mt-4">
+					</a>
+					<a href="https://www.linkedin.com/in/harshgupta940/">
+					<div className="dark:bg-gray-300 rounded-md h-20 w-48 my-4 mx-4 flex">
+						<h3 className="text-3xl font-semibold text-black mt-5 mx-6">
 							Harsh
 						</h3>
+						<svg className="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"> <path d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z"/></svg>
+					
 					</div>
-					<div className="dark:bg-gray-300 rounded-md h-20 w-48 my-4 mx-4">
-						<h3 className="text-3xl font-semibold text-black mt-4 ">
+					</a>
+					<a href="https://www.linkedin.com/in/pushpak-kumawat-b4bb921ba/">
+					<div className="dark:bg-gray-300 rounded-md h-20 w-48 my-4 mx-4 flex">
+						<h3 className="text-3xl font-semibold text-black mt-5 mx-4 ">
 							Pushpak
 						</h3>
+						<svg className="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"> <path d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z"/></svg>
+					
 					</div>
+					</a>
 				</div>
 			</div>
 		</section>
